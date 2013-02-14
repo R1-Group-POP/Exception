666eb9d1def9662035dc042a9f7940efa39dfb59
Patrick <pkkann@gmail.com>
2013-02-14 11:26:19 +0100

    tests rettet

--- Exception_School/test/MyStackTest.java
+++ Exception_School/test/MyStackTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.omg.PortableInterceptor.SUCCESSFUL;
 
 /**
  *
@@ -36,26 +37,55 @@ public class MyStackTest {
     }
 
     /**
+     * Test of push method exception, of class MyStack.
+     */
+    @Test (expected=StackIsFullException.class)
+    public void testPush_Exception() throws Exception {
+        System.out.println("push exception");
+        System.out.println("Pusing 3 times");
+        int i = 5;
+        MyStack instance = new MyStack();
+        
+        instance.push(i);
+        instance.push(i);
+        instance.push(i);
+    }
+    
+    /**
      * Test of push method, of class MyStack.
      */
     @Test
     public void testPush() throws Exception {
         System.out.println("push");
+        System.out.println("Pusing 2 times");
         int i = 5;
         MyStack instance = new MyStack();
+        
+        instance.push(i);
         instance.push(i);
     }
 
+    /**
+     * Test of pop method exception, of class MyStack.
+     */
+    @Test (expected=StackIsEmptyException.class)
+    public void testPop_Exception() throws Exception {
+        System.out.println("pop exception");
+        System.out.println("Popping one time");
+        MyStack instance = new MyStack();
+        
+        instance.pop();
+    }
+    
     /**
      * Test of pop method, of class MyStack.
      */
     @Test
     public void testPop() throws Exception {
         System.out.println("pop");
+        System.out.println("Popping one time");
         MyStack instance = new MyStack();
-        int expResult = 5;
-        instance.push(expResult);
-        int result = instance.pop();
-        assertEquals(expResult, result);
+        instance.push(5);
+        instance.pop();
     }
 }
